e46e6f3b0c6a2d3d86c6df81b4223f168b5ac5a9
Arnav Borborah <arnavborborah11@gmail.com>
2022-05-22 02:34:33 -0400

    Allow each workspace to have its own .stylua.toml
    This is done because the LSP server uses this file as a way to detect
    lua workspaces.

--- nvim/init.lua
+++ nvim/init.lua
@@ -197,9 +197,7 @@ require('cosmic-ui').setup()
 
 require('null-ls').setup {
   sources = {
-    require('null-ls').builtins.formatting.stylua.with {
-      extra_args = { '--config-path', vim.fn.expand '~/.config/nvim/.stylua.toml' },
-    },
+    require('null-ls').builtins.formatting.stylua,
   },
   on_attach = function(client, bufnr)
     if client.supports_method 'textDocument/formatting' then
