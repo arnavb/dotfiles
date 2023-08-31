9bd1b353802b5f2e2e3d348ce5fe0961a0f1d08e
Arnav Borborah <arnavborborah11@gmail.com>
2023-08-31 02:10:04 -0400

    Implement basic LSP setup based on kickstart.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -27,6 +27,21 @@ require("lazy").setup({
    'lewis6991/gitsigns.nvim',
    'numToStr/Comment.nvim',
    'andweeb/presence.nvim',
+
+	 -- LSP
+	 {
+		 'neovim/nvim-lspconfig',
+		 dependencies = {
+			 'williamboman/mason.nvim',
+			 'williamboman/mason-lspconfig.nvim',
+		 },
+	 },
+	 { 
+		 'hrsh7th/nvim-cmp',
+		dependencies = {
+			'hrsh7th/cmp-nvim-lsp',
+		},
+	}
 })
 
 vim.opt.mouse = ""
@@ -90,3 +105,52 @@ require('indent_blankline').setup {
 }
 require('Comment').setup()
 require('gitsigns').setup()
+
+require("mason").setup()
+require('mason-lspconfig').setup {
+	ensure_installed = {'lua_ls'}
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+require('mason-lspconfig').setup_handlers {
+	function (server_name)
+		require('lspconfig')[server_name].setup {
+			capabilities = capabilities
+		}
+	end
+}
+
+local cmp = require 'cmp'
+
+cmp.setup {
+  mapping = cmp.mapping.preset.insert {
+    ['<C-n>'] = cmp.mapping.select_next_item(),
+    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete {},
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+  },
+}
