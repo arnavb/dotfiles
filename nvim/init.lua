abf12609a5ec8a51db6488b6493917b337fe6ed1
Arnav Borborah <arnavborborah11@gmail.com>
2024-06-23 14:00:24 -0400

    Set up basic Python config

--- nvim/init.lua
+++ nvim/init.lua
@@ -104,6 +104,8 @@ local settings = {
       telemetry = { enable = false },
     },
   },
+  pyright = {},
+  black = {},
   stylua = {},
   luacheck = {},
 }
@@ -163,6 +165,7 @@ cmp.setup {
 require('conform').setup {
   formatters_by_ft = {
     lua = { 'stylua' },
+    python = { 'black' },
   },
   format_on_save = true,
 }
