c2b259eb0672e6144e3071d4120cd4537bdafc8c
Arnav Borborah <arnavborborah1@gmail.com>
2024-07-07 12:04:51 -0400

    Enable treesitter indentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -111,6 +111,9 @@ require('nvim-treesitter.configs').setup {
   highlight = {
     enable = true,
   },
+  indent = {
+    enable = true,
+  },
 }
 
 local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
