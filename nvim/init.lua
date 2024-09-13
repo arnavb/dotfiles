e77831944eb32978ee1a92be73ac1d42215e9c07
Arnav Borborah <arnavborborah11@gmail.com>
2024-09-13 14:58:38 -0400

    Add link to source for formatting commands

--- nvim/init.lua
+++ nvim/init.lua
@@ -314,6 +314,7 @@ if vim.fn.has 'wsl' ~= 0 then
 end
 
 -- Create commands to enable/disable formatting
+-- https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md#command-to-toggle-format-on-save
 vim.api.nvim_create_user_command('FormatDisable', function(args)
   if args.bang then
     -- FormatDisable! will disable formatting just for this buffer
